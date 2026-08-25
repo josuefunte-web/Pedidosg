bd938cca9b94c84962e4ba85bf3c456dba4b0a8c
josuefunte-web <josue.funte@gmail.com>
2026-08-25 17:06:18 +0200

    si

--- js/37-recetas.js
+++
@@ -1,4 +0,0 @@
-/* ═══════════════ RECETAS ═══════════════ */
-const REC_MESES=['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC'];
-const REC_PARTIDAS=['Calientes','Fríos','Postres','Bebidas','Aperitivos','Fondos','Otros'];
-const REC_TIPOS=['Principal','Entrante','Guarnición','Postre','Bebida','Otro'];
